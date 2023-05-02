6f3d92a80d02a100538c8deaf7acd2df09e06aec
Artur Karapetyan <artist1223kar@gmail.com>
2023-05-02 20:48:09 +0800

    movie-url

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -1,21 +1,20 @@
 import axios from 'axios';
 
 export default class ApiService {
+  url = 'https://api.themoviedb.org/3/';
   getData = async (request, page) => {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=d89e45a2906da5dce8ae832c65d913fe&query=${request}&page=${page}`
+      `${this.url}search/movie?api_key=d89e45a2906da5dce8ae832c65d913fe&query=${request}&page=${page}`
     );
     return response.data;
   };
   getGenre = async () => {
-    const response = await axios.get(
-      'https://api.themoviedb.org/3/genre/movie/list?api_key=d89e45a2906da5dce8ae832c65d913fe'
-    );
+    const response = await axios.get(`${this.url}genre/movie/list?api_key=d89e45a2906da5dce8ae832c65d913fe`);
     return response.data.genres;
   };
   createGuestSession = async () => {
     const response = await axios.get(
-      'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=d89e45a2906da5dce8ae832c65d913fe'
+      `${this.url}authentication/guest_session/new?api_key=d89e45a2906da5dce8ae832c65d913fe`
     );
     return response.data.guest_session_id;
   };
@@ -27,7 +26,7 @@ export default class ApiService {
       'Content-Type': 'application/json;charset=utf-8',
     };
     const response = await axios.post(
-      `https://api.themoviedb.org/3/movie/${id}/rating?api_key=d89e45a2906da5dce8ae832c65d913fe&guest_session_id=${sessionId}`,
+      `${this.url}movie/${id}/rating?api_key=d89e45a2906da5dce8ae832c65d913fe&guest_session_id=${sessionId}`,
       requestBody,
       { headers }
     );
@@ -36,7 +35,7 @@ export default class ApiService {
 
   getRatedMovies = async (sessionId) => {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/guest_session/${sessionId}/rated/movies?api_key=d89e45a2906da5dce8ae832c65d913fe&sort_by=created_at.asc`
+      `${this.url}guest_session/${sessionId}/rated/movies?api_key=d89e45a2906da5dce8ae832c65d913fe&sort_by=created_at.asc`
     );
     return response.data;
   };
